c3abca1fc1339323a75d7c2a7caae1f96c078e8c
Wojciech Szałapski <180706@edu.p.lodz.pl>
2014-11-29 15:16:51 +0100

    Drobne poprawki, przypadkiem nie uwzględniłem wszystkiego w poprzednich commitach.

--- dataModel/src/main/java/dataModel/KHint.java
+++ dataModel/src/main/java/dataModel/KHint.java
@@ -3,32 +3,38 @@ package dataModel;
 import java.awt.Image;
 
 public class KHint {
-	private String text;
-	private Image photo;
-	private int distance;
-	
-	public KHint(String text, Image photo, int distance) {
-		this.text = text;
-		this.photo = photo;
-		this.distance = distance;
-	}
-	
-	public String getText() {
-		return text;
-	}
-	public void setText(String text) {
-		this.text = text;
-	}
-	public Image getPhoto() {
-		return photo;
-	}
-	public void setPhoto(Image photo) {
-		this.photo = photo;
-	}
-	public int getDistance() {
-		return distance;
-	}
-	public void setDistance(int distance) {
-		this.distance = distance;
-	}
+
+    private String text;
+    private Image photo;
+    private int distance;
+
+    public KHint(String text, Image photo, int distance) {
+        this.text = text;
+        this.photo = photo;
+        this.distance = distance;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Image getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(Image photo) {
+        this.photo = photo;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
 }
